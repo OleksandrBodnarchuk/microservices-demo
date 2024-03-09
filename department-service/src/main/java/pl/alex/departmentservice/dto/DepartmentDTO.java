6f4de3a5aac535b2cbc349c8e2efe20c7663add3
Alex <oleksandr.bodnarchuk@co3.eu>
2024-03-09 13:36:14 +0100

    added Swagger api documentation do department service - closes #17

--- department-service/src/main/java/pl/alex/departmentservice/dto/DepartmentDTO.java
+++ department-service/src/main/java/pl/alex/departmentservice/dto/DepartmentDTO.java
@@ -1,11 +1,16 @@
 package pl.alex.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
+@Schema(
+    name = "DepartmentDTO",
+    description = "Department Model information"
+)
 @Builder
-public record DepartmentDTO(String name,
-                            String description,
-                            @NotBlank String code) {
+public record DepartmentDTO(@Schema(description = "Department name") String name,
+                            @Schema(description = "Department description") String description,
+                            @Schema(description = "Department code") @NotBlank String code) {
 
 }
